6ef96006973e0df28d3277ce2461d48a6f3eb6f6
Robin Unger <robin.e.unger@gmail.com>
2018-12-03 19:30:12 -0600

    updating exercises

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,5 +1,27 @@
-const snakeCase = function() {
+const snakeCase = function(str) {
+    if (isCamelCase(str)) {
+        let snakeCase = str;
+        let position = 0;
+        while (isCamelCase(snakeCase)) {
 
+            position = snakeCase.search(/[a-z][A-Z]/) + 1;
+            snakeCase = snakeCase.slice(0, position) + '_' + snakeCase.slice(position);
+            console.log(snakeCase);
+        }
+
+        return snakeCase.toLowerCase();
+
+    }
+
+        return str.toLowerCase()
+                                .replace(/[\s]/g, '_')
+                                .replace(/[^\w]/g, '')
+                                .replace(/[-]/g, '_');
+}
+
+const isCamelCase = function(str) {
+    return /[a-z][A-Z]/.test(str);
 }
 
 module.exports = snakeCase
+
