1797326d244e4dd9d1031b3493cac53b45692fdd
Robin Unger <robin.e.unger@gmail.com>
2018-12-05 19:36:40 -0600

    completed caesar

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -2,15 +2,23 @@ const caesar = function(str, shift) {
     if ( shift === 0) return str;
 
     let caesar = [];
-    str.split('').forEach(char => {
-        /[\w]/.test(char) ? caesar.push(String.fromCharCode(normalizeShift(char, shift))) : caesar.push(char);
+    str.split('').forEach(char => { /[\w]/.test(char) ? 
+        caesar.push(String.fromCharCode(normalizeShift(char, shift))) :
+        caesar.push(char);
     });
     return caesar.join('');
 }
 
 const normalizeShift = function(str, shift) {
     const code = str.charCodeAt();
-    // if str <= 0 do the opposite
+    
+    if (shift < 0) {
+        if (str === str.toUpperCase()) {
+            return (code - 'Z'.charCodeAt() + shift) % 26 + 'Z'.charCodeAt();
+        }
+        return (code - 'z'.charCodeAt() + shift) % 26 + 'z'.charCodeAt();
+    }
+
     if (str === str.toUpperCase()) {
         return (code - 'A'.charCodeAt() + shift) % 26 + 'A'.charCodeAt();
     }
