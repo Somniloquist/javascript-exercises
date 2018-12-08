d311432d9d9b46c7b680efaf716969089309b8e3
Robin Unger <robin.e.unger@gmail.com>
2018-12-08 17:58:34 -0600

    progress: complted fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,4 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let fibonacci = [0, 1, 1];
+    if (n < 0) return 'OOPS';
+    if (n <= 2) return fibonacci[n];
+
+    for (let i = 2; i < n; i++) {
+        const previous = fibonacci[i - 1];
+        const current = fibonacci[i];
+        fibonacci.push(previous + current);
+    }
+
+    return fibonacci[n];
 
 }
 
