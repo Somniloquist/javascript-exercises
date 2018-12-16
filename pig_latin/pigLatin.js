5894e0edefe7eca7b342577fc61a939a9c935052
Robin Unger <robin.e.unger@gmail.com>
2018-12-16 17:35:12 -0600

    refactored using array.map

--- pig_latin/pigLatin.js
+++ pig_latin/pigLatin.js
@@ -1,53 +1,22 @@
-function translate(str) {
-	const words = str.split(' ');
-	let translated = [];
+function translate(string) {
+	return string
+	.split(' ')
+	.map(word => {
+		const index = indexOfVowel(word);
+		const start = word.slice(0, index);
+		const end = word.slice(index);
+		return `${end}${start}ay`;
+	})
+	.join(' ');
 
-	words.forEach(word => {
-		let temp = word.split('');
-		// starts with a vowel
-		if (!isConsonant(word, 1)) {
-			temp.push('ay');
-		// starts with 3 consonants
-		} else if (word[0] === 'q' && word[1] === 'u') {
-			temp.push(word[0] + word[1] + 'ay');
-			temp.shift();
-			temp.shift();
-		} else if (word[1] === 'q' && word[2] === 'u') {
-			temp.push(word[0] + word[1] + word[2]+ 'ay');
-			temp.shift();
-			temp.shift();
-			temp.shift();
-		} else if (isConsonant(word, 3)) {
-			temp.push(word[0] + word[1] + word[2] + 'ay');
-			temp.shift()
-			temp.shift();
-			temp.shift();
-		// starts with 2 consonants
-		} else if (isConsonant(word, 2)) {
-			temp.push(word[0] + word[1] + 'ay');
-			temp.shift();
-			temp.shift();
-		//starts with consonant
-		} else {
-			temp.push(word[0] + 'ay');
-			temp.shift();
-		}
-
-		translated.push(temp.join(''));
-	});
-
-	return translated.join(' ');
 }
 
-// returns true if all characters up the index are consonants
-function isConsonant(word, index) {
-	const vowels = ['a', 'e', 'i', 'o', 'u'];
-	for (let i = 0; i < index; i++) {
-		if (vowels.includes(word[i])) {
-			return false;
-		}
+function indexOfVowel(word) {
+	const vowels = word.match(/[aeiou]/g);
+	if (vowels[0] === 'u' && word[word.indexOf(vowels[0]) - 1] === 'q') {
+		return word.indexOf(vowels[1]);
 	}
-	return true;
+	return word.indexOf(vowels[0]);
 }
 
 module.exports = {
